0fc0885f836916bf67b7ebb26fa9ed1a7fd32445
Jaegoomon <jaegoo1199@gmail.com>
2022-06-27 22:37:57 +0900

    💄 items 목록
    * thymeleaf 사용

---
+++ src/main/java/hello/itemservice/web/BasicItemController.java
@@ -0,0 +1,26 @@
+package hello.itemservice.web;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.ItemUpdateDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+}
