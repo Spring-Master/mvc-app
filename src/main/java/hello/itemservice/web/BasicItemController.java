9b3c114209956753ce711d273c62e3eebd42b2b1
Jaegoomon <jaegoo1199@gmail.com>
2022-06-28 10:32:20 +0900

    ✨ RedirectAttribute 적용
    * url 인코딩 지원
    * 쿼리 파라미터 적용

--- src/main/java/hello/itemservice/web/BasicItemController.java
+++ src/main/java/hello/itemservice/web/BasicItemController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -51,8 +52,10 @@ public class BasicItemController {
     }
 
     @PostMapping("/add")
-    public String postItem(@ModelAttribute("item") Item item) {
+    public String postItem(@ModelAttribute("item") Item item, RedirectAttributes redirectAttributes) {
         itemRepository.save(item);
-        return "redirect:/basic/items/" + item.getId();
+        redirectAttributes.addAttribute("itemId", item.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
     }
 }
