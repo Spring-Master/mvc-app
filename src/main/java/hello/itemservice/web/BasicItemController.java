757b28e396673329ef92a3f9be7faa070afec6f9
Jaegoomon <jaegoo1199@gmail.com>
2022-07-04 23:19:18 +0900

    ✨ 유효성 검증
    * Map을 통해서 유효성 검증
    * 오류 발생시 에러 정보와 함께 다시 폼을 렌더링
    * 타입에 대한 오류 검증 불가(Controller로 오기 전에 에러 발생)

--- src/main/java/hello/itemservice/web/BasicItemController.java
+++ src/main/java/hello/itemservice/web/BasicItemController.java
@@ -5,13 +5,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Slf4j
 @Controller
@@ -77,8 +75,39 @@ public class BasicItemController {
     }
 
     @PostMapping("/add")
-    public String postItem(@ModelAttribute("item") Item item, RedirectAttributes redirectAttributes) {
-        log.info("item.open={}", item.getOpen());
+    public String postItem(
+            @ModelAttribute("item") Item item,
+            RedirectAttributes redirectAttributes,
+            Model model
+    ) {
+
+        // Maps for saving validation errors
+        Map<String, String> errors = new HashMap<>();
+
+        // Validation logics
+        if (!StringUtils.hasText(item.getItemName())) {
+            errors.put("itemName", "상품이름은 필수입니다.");
+        }
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000_000) {
+            errors.put("price", "가격은 1,000 ~ 1,000,000 까지 허용합니다.");
+        }
+        if (item.getQuantity() == null || item.getQuantity() <= 0 || item.getQuantity() >= 9999) {
+            errors.put("quantity", "수량은 최대 9,999 까지 허용합니다.");
+        }
+
+        // Multi field validation logic
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int result = item.getPrice() * item.getQuantity();
+            if (result < 10000) {
+                errors.put("globalError", "가격 * 수량의 합은 10,000을 넘어야 합니다.");
+            }
+        }
+
+        if (!errors.isEmpty()) {
+            model.addAttribute("errors", errors);
+            return "basic/addForm";
+        }
+
         itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", item.getId());
         redirectAttributes.addAttribute("status", true);
